895cd68b5b2058b00e8a17b19aa99a35636910ae
Gmtn09093232 <157684016+Gmtn09093232@users.noreply.github.com>
2026-09-07 23:11:35 -0700

    Update server.js

--- server.js
+++ server.js
@@ -474,13 +474,12 @@ app.get('/api/project/:id', async(req,res)=>{
 });
 
 // Serve static files from the current directory (where server.js lives)
-app.use(express.static());
+app.use(express.static(__dirname));
 
 // Fallback: serve index.html for all other routes (including "/")
 app.get('/*splat', (req, res) => {
-  res.sendFile(path.join('index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
-
 // --- Start server ---
 app.listen(PORT, ()=>{
   console.log(`Engineering Cost Estimator V6 running on http://localhost:${PORT}`);
